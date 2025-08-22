04edac3ba3b6ad0f380d5b3a53319f7a43bd4658
Korovel Vladimir <106909929+VladimirKorolev1986@users.noreply.github.com>
2025-08-22 20:31:16 +0300

    Update 3.sql

--- WindowFunctions/HW/3.sql
+++ WindowFunctions/HW/3.sql
@@ -7,11 +7,13 @@
 
 -- Для выполнения запроса используйте таблицы titles и ratings. Поля к выводу: год (year_of_start), кумулятивная сумма (cum_cnt). Отсортируйте результат по их возрастанию.
 
-WITH cte AS (SELECT year_of_start, (SELECT avg_rating FROM ratings WHERE ratings.title_id=titles.id) AS r
+WITH cte AS (SELECT titles.year_of_start, count(titles.year_of_start) as cum_cnt
   FROM titles
- WHERE ((SELECT avg_rating FROM ratings WHERE ratings.title_id=titles.id) IS NOT NULL) AND ((SELECT avg_rating FROM ratings WHERE ratings.title_id=titles.id) = 10.00)
- ORDER BY year_of_start)
+  JOIN ratings
+    ON titles.id=ratings.title_id
+ WHERE ratings.avg_rating =10
+ GROUP BY titles.year_of_start)
  
  SELECT year_of_start, 
- COUNT(year_of_start) OVER (PARTITION BY year_of_start ORDER BY 2) AS cum_cnt
+ SUM(cum_cnt) OVER (ORDER BY year_of_start) AS cum_cnt
  FROM cte
